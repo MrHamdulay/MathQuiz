bf55e297ecf5b18401ae4f50b113512f6f36b14b
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2013-02-22 09:13:32 +0200

    dont show correct or incorrect if user didnt even answer

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 def generate_question(level=0):
     operations = '+-'
-    size = 3
-    question = (random.randint(1, 30) if i % 2 == 0 else random.choice(operations) for i in xrange(size*2-1))
+    size = 2
+    question = (random.randint(1, 15) if i % 2 == 0 else random.choice(operations) for i in xrange(size*2-1))
     return ' '.join(map(str, question))
 
 @app.route('/')
@@ -26,15 +26,15 @@ class State:
         self.incorrectlyAnswered = incorrectlyAnswered
 
     def updateState(self, response):
-        response.set_cookie(QUESTIONS_REMAINING_COOKIE, self.questionsRemaining)
-        response.set_cookie(CORRECTLY_ANSWERED_COOKIE, self.correctlyAnswered)
-        response.set_cookie(INCORRECTLY_ANSWERED_COOKIE, self.incorrectlyAnswered)
+        response.set_cookie(State.QUESTIONS_REMAINING_COOKIE, self.questionsRemaining)
+        response.set_cookie(State.CORRECTLY_ANSWERED_COOKIE, self.correctlyAnswered)
+        response.set_cookie(State.INCORRECTLY_ANSWERED_COOKIE, self.incorrectlyAnswered)
 
     @staticmethod
-    def fromCookies(self, request):
-        questionsRemaining = request.cookies.get(QUESTIONS_REMAINING_COOKIE, 5)
-        correctlyAnswered = request.cookies.get(CORRECTLY_ANSWERED_COOKIE, 0)
-        incorrectlyAnswered = request.cookies.get(INCORRECTLY_ANSWERED_COOKIE, 0)
+    def fromCookies(request):
+        questionsRemaining = int(request.cookies.get(State.QUESTIONS_REMAINING_COOKIE, 5))
+        correctlyAnswered = int(request.cookies.get(State.CORRECTLY_ANSWERED_COOKIE, 0))
+        incorrectlyAnswered = int(request.cookies.get(State.INCORRECTLY_ANSWERED_COOKIE, 0))
 
         return State(questionsRemaining, correctlyAnswered, incorrectlyAnswered)
 
@@ -56,7 +56,12 @@ def quiz(**kwargs):
     # number of questions remaining in quiz
     # if we still have to ask questions of the user
     if state.questionsRemaining - 1 != 0:
-        response = make_response(render_template('quiz.html', numberRemaining=state.questionsRemaining, question=generate_question(), result = result, correct=correctlyAnswered))
+        response = make_response(render_template('quiz.html',
+            numberRemaining=state.questionsRemaining,
+            question=generate_question(),
+            answered = result != '',
+            correct=correctlyAnswered))
+
         # decrease remaining questions counter if the user answered the question
         if result:
             state.questionsRemaining -= 1
