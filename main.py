c02e9d1394df4e9fcb0c6008ba836f1d988fbd4f
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2013-02-22 08:37:20 +0200

    Cookie state is stored in some class and handled just by that. Do correct checking thing (EVAL??? FIX THIS)

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import random
 
 app = Flask(__name__)
 
-QUESTIONS_REMAINING_COOKIE = 'questionsRemaining'
 
 def generate_question(level=0):
     operations = '+-'
@@ -16,19 +15,55 @@ def generate_question(level=0):
 def index():
     return render_template('index.html')
 
+class State:
+    QUESTIONS_REMAINING_COOKIE = 'questionsRemaining'
+    CORRECTLY_ANSWERED_COOKIE = 'correctlyAnswered'
+    INCORRECTLY_ANSWERED_COOKIE = 'incorrectlyAnswered'
+
+    def __init__(self, questionsRemaining = 5, correctlyAnswered = 0, incorrectlyAnswered = 0):
+        self.questionsRemaining = questionsRemaining
+        self.correctlyAnswered = correctlyAnswered
+        self.incorrectlyAnswered = incorrectlyAnswered
+
+    def updateState(self, response):
+        response.set_cookie(QUESTIONS_REMAINING_COOKIE, self.questionsRemaining)
+        response.set_cookie(CORRECTLY_ANSWERED_COOKIE, self.correctlyAnswered)
+        response.set_cookie(INCORRECTLY_ANSWERED_COOKIE, self.incorrectlyAnswered)
+
+    @staticmethod
+    def fromCookies(self, request):
+        questionsRemaining = request.cookies.get(QUESTIONS_REMAINING_COOKIE, 5)
+        correctlyAnswered = request.cookies.get(CORRECTLY_ANSWERED_COOKIE, 0)
+        incorrectlyAnswered = request.cookies.get(INCORRECTLY_ANSWERED_COOKIE, 0)
+
+        return State(questionsRemaining, correctlyAnswered, incorrectlyAnswered)
+
 @app.route('/quiz/<level>', methods=('get', 'post'))
 def quiz(**kwargs):
-    lastQuestion = request.args.get('question', None)
+    lastQuestion = request.form.get('question', None)
+    result = request.form.get('result', '')
+
+    correctlyAnswered = None
+    if lastQuestion:
+        correctlyAnswered = eval(lastQuestion) == result
+        if correctlyAnswered:
+            state.correctlyAnswered += 1
+        else:
+            state.incorrectlyAnswered += 1
+
+    state = State.fromCookies(request)
 
     # number of questions remaining in quiz
-    questionsRemaining = int(request.cookies.get(QUESTIONS_REMAINING_COOKIE, 6))
     # if we still have to ask questions of the user
-    if questionsRemaining != 0:
-        response = make_response(render_template('quiz.html', numberRemaining=questionsRemaining, question=generate_question()))
-        # decrease remaining questions counter
-        response.set_cookie(QUESTIONS_REMAINING_COOKIE, questionsRemaining-1)
+    if state.questionsRemaining - 1 != 0:
+        response = make_response(render_template('quiz.html', numberRemaining=state.questionsRemaining, question=generate_question(), result = result, correct=correctlyAnswered))
+        # decrease remaining questions counter if the user answered the question
+        if result:
+            state.questionsRemaining -= 1
     else:
         response = make_response(render_template('quizComplete.html'))
+
+    state.updateState(response)
     return response
 
 @app.route('/leaderboard')
