a9d293ca12fa6f2b39a69c034c1ba3db877fc1b6
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2013-02-24 20:36:04 +0200

    Method for multiplication and division

--- question.py
+++ question.py
@@ -6,6 +6,15 @@ class Enum(set):
             return name
         raise AttributeError
 
+class Question:
+    def __init__(self, first, operation, second):
+        self.answer = eval(str(first)+str(operation)+str(second), {}, {})
+        self.first = first
+        self.second = second
+
+    def __str__(self):
+        return '%d %s %d' % (self.first, self.operation, self.second)
+
 difficulties = Enum('EASY', 'MEDIUM', 'HARD')
 types = Enum('ADDSUB', 'MULTDIV')
 
@@ -13,16 +22,15 @@ def generateAddSub(difficulty):
     operations = '+-'
     operation = random.choice(operations)
 
-    answer = None
     if difficulty == difficulties.EASY:
         first = random.randint(1, 15)
         second = random.randint(1, 15)
 
         answer = eval(first+operation+second, {}, {})
-        # negative numbers is hard, we don't deal
+        # negative numbers is hard, we can't deal
         if answer < 0:
-            answer *= -1
-            first, second = second, first
+            second, first = first, second
+
     elif difficulty == difficulties.MEDIUM:
         first = random.randint(11, 50)
         second = random.randint(11, 60-first)
@@ -30,13 +38,38 @@ def generateAddSub(difficulty):
         first = random.randint(11, 80)
         second = random.randint(11, 100-first)
 
-    answer = eval(first+operation+second, {}, {})
-
-    return (first, operation, second), answer
+    return Question(first, operation, second)
 
 def generateMultDiv(difficulty):
     operations = '/x'
     operation = random.choice(operations)
+
+    first, second = None, None
+
+    if operation == '/':
+        if difficulty == difficulties.EASY:
+            result = random.randint(0, 10)
+            second = random.randint(0, 5)
+        elif difficulty == difficulties.MEDIUM:
+            result = random.randint(0, 12)
+            second = random.randint(0, 20)
+        elif difficulty == difficulties.HARD:
+            result = random.randint(-12, 12)
+            second = random.randint(-20, 20)
+
+        first = result*second
+    elif operation == '*':
+        if difficulty = difficulties.EASY:
+            first = random.randint(0, 8)
+            second = random.randint(0, 10/first)
+        elif difficulty = difficulties.MEDIUM:
+            first = random.randint(0, 12)
+            second = random.randint(0, 80/first)
+        elif
+            first = random.randint(-12, 12)
+            second = random.randint(-80/first, 80/first)
+
+    return Question(first, operation, second)
 
 
 def generateQuestion(type, difficulty):
