c4eec38a1737b8a64196e986f6f4dc699011a0be
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2013-03-16 23:09:01 +0200

    add analytics queue to drop script

--- drop_db.py
+++ drop_db.py
@@ -15,6 +15,7 @@ c.execute('drop table if exists quiz')
 c.execute('drop table if exists quiz_submissions')
 c.execute('drop table if exists app_settings')
 c.execute('drop table if exists feedback')
+c.execute('drop table if exists analytics_queue')
 
 c.close()
 db.commit()
