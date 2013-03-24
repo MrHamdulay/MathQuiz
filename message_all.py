73907ff95ba3937649387647022700a5fada8327
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2013-03-24 17:58:35 +0100

    reorder connections so they make sense

--- message_all.py
+++ message_all.py
@@ -1,29 +1,29 @@
 from mathquiz import config, database
 
-import MxitApi
+from MxitAPI import MxitAPI
 
 if __name__ == '__main__':
-    print 'connecting to mxit API'
-    api=MxitApi(config.client_id, config.secret_id, 'mathchallenge')
-    api.auth(('message/send',))
-    print 'connected'
+
+    message = None
+    with open(raw_input('Enter path of message you wanna send:'), 'r') as f:
+        message = f.read()
+    i = raw_input('Message a single or all user(s)? (single / all)')
 
     print 'connecting to database'
     db = database.create_database()
     c = db.cursor()
     print 'connected'
 
-    i = raw_input('Message a single or all user(s)? (single / all)')
     if i[0] == 's':
         identifier = raw_input('Enter username or userid')
         c.execute('SELECT username, mxit_userid FROM users WHERE mxit_userid = %s or username = %s', (identifier, identifier))
     elif i[0] == 'a':
         c.execute('SELECT username, mxit_userid FROM users')
 
-    message = None
-    with open(raw_input('Enter path of message you wanna send:'), 'r') as f:
-        message = f.read()
-
+    print 'connecting to mxit API'
+    api=MxitAPI(config.client_id, config.secret_id, 'mathchallenge')
+    api.auth(('message/send',))
+    print 'connected'
 
     for username, userid in c:
         message.replace('<user>', username)
