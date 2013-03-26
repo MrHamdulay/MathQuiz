ea44eca2d294ddbc2e630801715a71c73494fc80
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2013-03-26 12:19:30 +0200

    port number is a parameter option

--- run.py
+++ run.py
@@ -1,20 +1,29 @@
 import sys
-from mathquiz import app, config
+import argparse
+
 from raven.contrib.flask import Sentry
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 
-if config.sentry_enabled:
-    sentry = Sentry(app, dsn=config.sentry_dsn)
+from mathquiz import app, config
 
-if 'dev' in sys.argv:
-    print 'Running in development mode'
-    app.run(debug=True, host='0.0.0.0', use_debugger=True)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('MathChallenge')
+    parser.add_argument('-p', '--port', help='Port number to listen on', nargs=1, default=config.port, type=int)
+    parser.add_argument('--dev', help='Are we running the service in developer mode?', action='store_const', const=True)
+    args = parser.parse_args()
+    args.port = args.port[0]
 
-else:
-    from tornado.wsgi import WSGIContainer
-    from tornado.httpserver import HTTPServer
-    from tornado.ioloop import IOLoop
+    if config.sentry_enabled:
+        sentry = Sentry(app, dsn=config.sentry_dsn)
 
-    print 'Running in production mode'
-    http_server = HTTPServer(WSGIContainer(app))
-    http_server.listen(config.port)
-    IOLoop.instance().start()
+    if args.dev:
+        print 'Running in development mode'
+        app.run(debug=True, host='0.0.0.0', port=args.port, use_debugger=True)
+    else:
+        print 'Running in production mode'
+        http_server = HTTPServer(WSGIContainer(app))
+        http_server.listen(args.port)
+        IOLoop.instance().start()
+
