67e1b9996e8c0b19c6c9319e753bb1b17a02f979
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2013-03-27 18:40:23 +0100

    Revert "updated scripts to work with python 2.7"
    This reverts commit 6ddebd5ead78b26c257e6e1f6b73ebdb6aa19275.

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
-virtualenv-2.7 venv
+virtualenv venv
 . venv/bin/activate
 git pull origin master
 
 venv/bin/pip install -r requirements.txt
+
+python pyga/setup.py install
+python create_db.py
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd `dirname $0`
 . venv/bin/activate
-exec python2.7 run.py $@
+exec python run.py $@
