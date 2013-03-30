80b82a2c858a5a8ae11760b53c80c222134425df
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2013-03-31 00:00:08 +0200

    PEP 8 create_db.py

--- create_db.py
+++ create_db.py
@@ -8,55 +8,55 @@ c = db.cursor()
 
 try:
     c.execute('CREATE TABLE users ('
-                'id SERIAL,'
-                'username VARCHAR UNIQUE,'
-                'difficulty INTEGER DEFAULT 0,'
-                'mxit_userid varchar(100) UNIQUE,'
-                'joined_date timestamp DEFAULT NOW()'
-                ')')
+              'id SERIAL,'
+              'username VARCHAR UNIQUE,'
+              'difficulty INTEGER DEFAULT 0,'
+              'mxit_userid varchar(100) UNIQUE,'
+              'joined_date timestamp DEFAULT NOW()'
+              ')')
     c.execute('CREATE TABLE users_highscores ('
-                'id SERIAL,'
-                'userid INTEGER,'
-                'difficulty INTEGER,' # add ten for streak stuff
-                'highscore INTEGER NOT NULL DEFAULT 0'
-                ')')
+              'id SERIAL,'
+              'userid INTEGER,'
+              'difficulty INTEGER,'  # add ten for streak stuff
+              'highscore INTEGER NOT NULL DEFAULT 0'
+              ')')
     c.execute('CREATE UNIQUE INDEX highscores ON users_highscores (userid, difficulty)')
     c.execute('create index highscores_by_difficulty on users_highscores (difficulty)')
 
     c.execute('CREATE TABLE quiz ('
-                'id SERIAL,'
-                'type varchar(20),'
-                'start_time timestamp,'
-                'user_id integer,' #user id from users
-                'num_correct integer,'
-                'num_questions integer,'
-                'score integer,'
-                'end_time timestamp)')
+              'id SERIAL,'
+              'type varchar(20),'
+              'start_time timestamp,'
+              'user_id integer,'  # user id from users
+              'num_correct integer,'
+              'num_questions integer,'
+              'score integer,'
+              'end_time timestamp)')
     c.execute('CREATE INDEX complete_time ON quiz ((end_time - start_time))')
 
     c.execute('CREATE TABLE quiz_submissions ('
-                'id SERIAL,'
-                'user_id INTEGER,'
-                'quiz_id INTEGER,'
-                'question VARCHAR(50),'
-                'answer INTEGER,'
-                'correct BOOLEAN,'
-                'score INTEGER,'
-                'submit_time TIMESTAMP DEFAULT NOW()'
-                ')') #user given answer, not necessarily correct
+              'id SERIAL,'
+              'user_id INTEGER,'
+              'quiz_id INTEGER,'
+              'question VARCHAR(50),'
+              'answer INTEGER,'
+              'correct BOOLEAN,'
+              'score INTEGER,'
+              'submit_time TIMESTAMP DEFAULT NOW()'
+              ')')  # user given answer, not necessarily correct
     c.execute('CREATE INDEX streak ON quiz_submissions (user_id, submit_time, correct)')
     c.execute('CREATE index quiz_submissions_by_id ON quiz_submissions (quiz_id)')
 
     c.execute('CREATE TABLE app_settings ('
-                'key VARCHAR, '
-                'value VARCHAR)')
+              'key VARCHAR, '
+              'value VARCHAR)')
     c.execute("INSERT INTO app_settings (key, value) VALUES ('schema_version', '%s')", (SCHEMA_VERSION, ))
 
     c.execute('CREATE TABLE feedback (username VARCHAR,'
-                    'userid INTEGER,'
-                    'feedback TEXT, '
-                    'submitted_date TIMESTAMP DEFAULT NOW()'
-                    ')')
+              'userid INTEGER,'
+              'feedback TEXT, '
+              'submitted_date TIMESTAMP DEFAULT NOW()'
+              ')')
 
     c.execute('CREATE TABLE analytics_queue (id SERIAL, data TEXT)')
 
