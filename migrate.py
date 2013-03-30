ff0e1ebeacc6109bec1bc35156f606c5aeac8149
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2013-03-30 23:46:57 +0200

    Script to give us DB migrations

---
+++ migrate.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import os
+import argparse
+from mathquiz import database
+
+MIGRATION_DIR = 'migrations/'
+MIGRATION_TABLE_SQL = 'CREATE TABLE migrations (id SERIAL, name VARCHAR'\
+                      ', performed TIMESTAMP DEFAULT NOW());'
+
+
+def createMigrationsTable(db):
+    c = db.cursor()
+    c.execute('select exists(select * from information_schema.tables'
+              ' where table_name=\'migrations\')')
+    exists = c.fetchone()[0]
+    if not exists:
+        c.execute(MIGRATION_TABLE_SQL)
+        db.commit()
+        print 'Created migration table'
+
+    c.close()
+
+
+def getDbMigrations(db):
+    c = db.cursor()
+    c.execute('SELECT name FROM migrations')
+    res = set(c)
+    c.close()
+
+    return res
+
+
+def getFsMigrations():
+    return set(filename for filename in os.listdir(MIGRATION_DIR)
+               if filename.endsWith('.sql'))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('MathChallenge migrator')
+    # if this argument isn't present we're only doing a dry run
+    parser.add_argument('--run', help='Do the real deal and migrate',
+                        action='store_const', const=True)
+    args = parser.parse_args()
+    dryRun = args.run
+
+    db = database.connect_postgres()
+
+    createMigrationsTable(db)
+
+    dbMigrations = getDbMigrations(db)
+    fsMigrations = getFsMigrations()
+
+    incompleteMigrations = fsMigrations - dbMigrations
+
+    c = db.cursor()
+    for i, migration in enumerate(incompleteMigrations):
+        print 'Running migration %d: %s' % (i, migration)
+        if dryRun:
+            print '(not really)'
+        else:
+            with open(migration) as f:
+                c.execute(f.read())
+                c.execute('INSERT INTO migrations (name) VALUES (%s)',
+                          (migration,))
+                print 'Migration %d complete' % i
+
+                db.commit()
+
+    c.close()
+    db.commit()
+
+    print 'All migrations complete'
