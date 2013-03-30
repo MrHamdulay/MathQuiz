64e4118ad1696fa4d5ef1224686fd09bc7b4049a
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2013-03-30 23:08:39 +0200

    PEP 8 Index view

--- mathquiz/views/index.py
+++ mathquiz/views/index.py
@@ -1,17 +1,17 @@
 from flask import session, render_template
 from mathquiz import app, analytics, database
 
+
 @app.route('/')
 def index():
     session['quizId'] = -1
-    analytics.track('page', {'page':'index'})
+    analytics.track('page', {'page': 'index'})
 
     # if the user has not given us a username we should probably ask for one
     return render_template('index.html',
-            username=session['username'],
-            userId=session['userId'],
-            current_difficulty=session['difficulty'].title(),
-            actual_difficulty=database.fetch_user_difficulty(session['userId']).title(),
-            rank=database.fetch_user_rank(session['userId'], session['difficulty']),
-            total_users=database.fetch_number_users()
-        )
+                           username = session['username'],
+                           userId = session['userId'],
+                           current_difficulty = session['difficulty'].title(),
+                           actual_difficulty = database.fetch_user_difficulty(session['userId']).title(),
+                           rank = database.fetch_user_rank(session['userId'], session['difficulty']),
+                           total_users = database.fetch_number_users())
